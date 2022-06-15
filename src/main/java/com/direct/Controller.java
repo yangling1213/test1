68796c8088dcd8be3db67e81f2cf960d9f3dbf39
direct.com <direct.com@qq.com>
2022-06-15 15:09:55 +0800

    second commit

--- src/main/java/com/direct/Controller.java
+++ src/main/java/com/direct/Controller.java
@@ -4,7 +4,9 @@ public class Controller {
     public static void main(String[] args) {
         System.out.println("git1........");
         System.out.println("test............");
-        System.out.println("first commit.............");
+        System.out.println("text commit.............");
         System.out.println("nihao............");
+        System.out.println("first commit......");
+
     }
 }
