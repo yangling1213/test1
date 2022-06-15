c92cac02739560fd5b4e7b09cbc0ff7acf618e25
direct.com <direct.com@qq.com>
2022-06-15 15:07:57 +0800

    fist commit

---
+++ src/main/java/com/direct/Controller.java
@@ -0,0 +1,10 @@
+package com.direct;
+
+public class Controller {
+    public static void main(String[] args) {
+        System.out.println("git1........");
+        System.out.println("test............");
+        System.out.println("first commit.............");
+        System.out.println("nihao............");
+    }
+}
